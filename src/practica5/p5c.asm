6e1a224ae17b4cb0ba3f0b9122617c25013a5e92
kvn-huesca <itswezk@gmail.com>
2026-04-07 02:23:54 -0700

    guardando

---
+++ src/practica5/p5c.asm
@@ -0,0 +1,21 @@
+%include "../../lib/pc_io.inc"  	; incluir declaraciones de procedimiento externos
+									; que se encuentran en la biblioteca libpc_io.a
+
+section	.text
+	global _start       ;referencia para inicio de programa
+	
+_start:                   
+	mov edx, msg		; edx = dirección de la cadena msg
+	call puts			; imprime cadena msg terminada en valor nulo (0)
+
+    mov byte [edx + 26], '@'  ;REGISTRO (ebx) + DESPLAZAMIENTO (26). Cambia el '0' por '@'
+
+    mov edx, msg        ; volver a apuntar a msg
+    call puts
+
+	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
+	int	0x80        	; llamada al sistema - fin de programa
+
+section	.data
+    msg	db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0 
+
---
+++ src/practica5/p5f.asm
@@ -0,0 +1,24 @@
+%include "../../lib/pc_io.inc"  	; incluir declaraciones de procedimiento externos
+									; que se encuentran en la biblioteca libpc_io.a
+
+section	.text
+	global _start       ;referencia para inicio de programa
+	
+_start:                   
+	mov edx, msg		; edx = dirección de la cadena msg
+	call puts			; imprime cadena msg terminada en valor nulo (0)
+
+    mov eax, msg
+    mov ebx, 19
+    mov byte [eax + ebx * 1], '%'  ;REGISTRO (edx) + INDICE (ebx = 25). Cambia 'z' por 'Z'
+
+    mov edx, msg        ; volver a apuntar a msg
+    call puts
+
+    mov ebx, 0
+	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
+	int	0x80        	; llamada al sistema - fin de programa
+
+section	.data
+    msg	db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0 
+
